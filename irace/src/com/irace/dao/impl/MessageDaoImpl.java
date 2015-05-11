9857ccf18da1b7cb4ce5db73f86dcc6a057ba6c4
12301107-wangjunmeng <12301107@bjtu.edu.cn>
2015-05-11 23:27:40 +0800

    MessageDaoImpl 实现

--- irace/src/com/irace/dao/impl/MessageDaoImpl.java
+++ irace/src/com/irace/dao/impl/MessageDaoImpl.java
@@ -66,8 +66,17 @@ public class MessageDaoImpl extends SDao implements MessageDao{
 	@Override
 	public List getMessageList(int userId, int pageNo, int pageItemNum,
 			boolean isRead) {
-		// TODO Auto-generated method stub
-		return null;
+		if(isRead){
+			this.hql = "FROM MessageEntity AS m inner join fetch sUserEntity AS s inner join fetch rUserEntity AS r WHERE m.receiver = ? and m.status = 1";
+		}
+		else{
+			this.hql = "FROM MessageEntity AS m inner join fetch sUserEntity AS s inner join fetch rUserEntity AS r WHERE m.receiver = ? and m.status = 0";
+		}
+		Query query = this.sessionFactory.getCurrentSession().createQuery(this.hql);
+		query.setInteger(0, userId);
+		query.setFirstResult((pageNo - 1) * pageItemNum);
+		query.setMaxResults(pageItemNum);
+		return query.list();
 	}
 
 }
